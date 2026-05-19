fa4c8cea3a25557b5f455817e4db831303d1d4e4
debs <dcssousa8@gmail.com>
2026-05-19 11:37:34 -0300

    - finished train functions and started shaping it into the main

--- src/plotters/get_plots.py
+++ src/plotters/get_plots.py
@@ -1,14 +1,37 @@
 # imports
-
+import matplotlib.pyplot as plt
+import pandas as pd
 ##############################################################################################
 
-def draw_tree()->None:
-    dot_data = StringIO()
-    export_graphviz(clf, out_file=dot_data,
-                    feature_names=feature_cols,
-                    class_names=class_names,
-                    filled=True, rounded=True,
-                    special_characters=True)
-    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-    graph.write_png('decision_tree.png')
-    pass
+def plot_cv_metrics(scores_df: pd.DataFrame) -> None:
+    """
+    Plot train and test cross-validation metrics as simple boxplots.
+    """
+    train_cols = ["train_recall", "train_precision", "train_accuracy"]
+    test_cols = ["test_recall", "test_precision", "test_accuracy"]
+    metric_labels = ["Recall", "Precision", "Accuracy"]
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+
+    axes[0].boxplot(scores_df[train_cols], tick_labels=metric_labels, showmeans=True)
+    axes[0].set_title("Train")
+    axes[0].set_ylabel("Score")
+
+    axes[1].boxplot(scores_df[test_cols], tick_labels=metric_labels, showmeans=True)
+    axes[1].set_title("Test")
+
+    plt.tight_layout()
+    plt.show()
+
+#TODO: fix tree plotting here
+
+# def draw_tree()->None:
+#     dot_data = StringIO()
+#     export_graphviz(clf, out_file=dot_data,
+#                     feature_names=feature_cols,
+#                     class_names=class_names,
+#                     filled=True, rounded=True,
+#                     special_characters=True)
+#     graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+#     graph.write_png('decision_tree.png')
+#     pass
